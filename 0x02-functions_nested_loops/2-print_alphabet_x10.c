5d385fea3245bcf6d8c7385caf93741f21e0af0a
Victoria Johnson <johnsonvickie884@gmail.com>
2022-09-13 07:33:35 -0700

    Modified 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function to print abc 10 times
+ * print_alphabet_X10 - function to print abc 10 times
  * Return: 0
  */
-void print_alphabet_x10(void);
+void print_alphabet_X10(void)
 {
 char c, i;
 
