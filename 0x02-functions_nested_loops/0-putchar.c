1e6a9ba4b8fbf3cad634e0e5dd7097258492f131
Victoria Johnson <johnsonvickie884@gmail.com>
2022-09-13 05:08:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar followed by a new line
+ * Description: using the main function
+ * this program prints out _putchar
+ * Return: 0
+ */
+
+int main(void);
+{
+char ch = '_putchar\n';
+putchar(ch);  
+return (0);  
+}
