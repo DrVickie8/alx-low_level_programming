d92c737e722a6d6e1a40c785e332a6e2ee709911
Victoria Johnson <johnsonvickie884@gmail.com>
2022-09-15 20:26:24 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,9 +4,12 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int, int);
 
-#endif /*MAIN_H*/
+int largest_number(int, int, int);
+
+void print_remaining_days(int, int, int);
+
+int convert_day(int, int);
+
+#endif /* MAIN_H */
 
