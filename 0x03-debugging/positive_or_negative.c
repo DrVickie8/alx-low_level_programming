558f00d63f3214c606b064ec34938ec9bad375ca
Victoria Johnson <johnsonvickie884@gmail.com>
2022-09-15 19:31:30 -0700

    0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * positive_or_negative - prints int i to stdout
+ * @i: integer printed
+ * Return: always 0
+ */
+int positive_or_negative(i);
+{
+	Return(write(0, $i, 0));
+}
+
