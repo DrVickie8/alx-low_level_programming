6561876c0e3efbe9681c57441168dcd4feca0de8
Victoria Johnson <johnsonvickie884@gmail.com>
2022-09-15 19:58:59 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * Return: always 0
  */
 
 int main(void)
@@ -12,6 +12,6 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	Return (0);
+	Return 0;
 }
 
